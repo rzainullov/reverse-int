631eb8f131b080ad670f502ae28f322b71d2b18d
rzainullov <rashid.zainullov@gmail.com>
2020-10-20 03:20:41 +0400

    finish task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = function reverse (n) {
-    let arr = n.split('');
+    let arr = n.toString().split('');
     if(arr[0] === "-")arr.shift();
     if(arr[arr.length-1] === "0")arr.pop();
     arr = arr.reverse();
-    return n = arr.join('')
+    return n = parseInt(arr.join(''))
 }
+
